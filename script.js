8c98f47a590c696b932a56ab0841f5da764006cf
Tyler Jenkins <tyjenki@gmail.com>
2019-12-18 22:31:33 -0500

    added new time of day function called 'colorChange()' that won't display the time but will be used to change the <textarea> background color.

--- script.js
+++ script.js
@@ -5,14 +5,43 @@ $(document).ready(function(){
     localStorage.setItem(time, value);
   });
 
-
-  function update() {
-    $("#currentDay").html(moment().format("MMMM Do YYYY hh:mm:ss a"));
-    setInterval(update, 1000);
+  //function to run moment.js and show Month Day Year Time
+  function dayTime() {
+    $("#currentDay").html(moment().format("MMMM Do YYYY HH:mm:ss a"));
+    setInterval(dayTime, 1000);
   };
   
-  update(); 
+  //run moment.js on page load
+  dayTime(); 
+
+
+  //function to change textarea color based on time of day
+  function colorChange() {
+    var d = new Date();
+    var n = d.getHours();
+    $("textarea").css("background-color", "#77dd77");
+    // if (n < 9) {
+    //   $("textarea").css("background-color", "#77dd77");
+    // }
+    // else if (n = 9)
+
+    // if (n >=10 && n < 18) {
+    //   // If time is after 10AM or before 6PM, apply .past 
+    //   document.body.className = "emeanight";
+    // }
+    // else {
+    //   // Else use ‘day’ theme
+    //   document.body.className = "emeaday";
+    // }
+    // setInterval(colorChange, 1000);
+  }
   
+  //run color changing function on page load
+  colorChange();
+
+ 
+  
+  // is there a way to consolidate this with an object?
   var hourNine = localStorage.getItem("hour-9");
   $("#text9").text(hourNine);
   
@@ -40,6 +69,33 @@ $(document).ready(function(){
   var hourFive = localStorage.getItem("hour-5");
   $("#text5").text(hourFive);
 
+
 });
 
-// next/last requirement:  Each timeblock is color coded to indicate whether it is in a past, present, or future hour.
+/*==============================
+
+Could run these functions every hour on the hour:
+
+function its10am() {
+  $("#hour-9").addClass("past");
+  $("#hour-9").removeClass("present");
+  $("#hour-10").addClass("present");
+  $("#hour-10").removeClass("future");
+}
+
+function its11am() {
+  $("#hour-10").addClass("past");
+  $("#hour-10").removeClass("present");
+  $("#hour-11").addClass("present");
+  $("#hour-11").removeClass("future");
+}
+================================*/
+
+
+/* 
+  dynamically add/remove class of element. setInterval to check every 1000ms and when condition met, change element class. What is that condition?
+  
+  "present" doesn't know the time - it just knows it's not past or future. SO when 9am block turns gray, turn 10am block red and 11 and later should be green until 6pm when everything then becomes future again
+    */
+
+  
